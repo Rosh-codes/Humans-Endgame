aa6d9f330aba774bf0cbfdcc4ad1526113740b2d
Roshan Adithya Jegadees <roshanadithya965@gmail.com>
2025-09-27 19:06:12 +0300

    added conditional rendering to new game button

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,73 @@ import { useState } from "react"
 import Header from "./components/Header"
 import people from "./components/people"
 import clsx from "clsx"
-export default function App(){
-  const content = people.map(role=>(
-    <div className="Humans-box" style={{backgroundColor : role.backgroundColor , color:role.color}} key={role.name}>
+
+export default function App() {
+  const [Guess, setGuess] = useState("firewall")
+  const [clickedKey, setClickedKey] = useState([])
+
+  const WrongGuessCount = clickedKey.filter(keys => !Guess.includes(keys)).length
+  const content = people.map((role,index) => (
+    <div
+      className={` Humans-box ${index < WrongGuessCount ? "lost" : ""}`}
+      style={{ backgroundColor: role.backgroundColor, color: role.color }}
+      key={role.name}
+    >
       <span>{role.icon}</span>
-      {role.name}
+      <span>{role.name}</span>
     </div>
-  )
-  )
-  const [Guess,setGuess]=useState("firewall")
-  const [clickedKey,setClickedKey]=useState([])
-  const WrongGuessKey = clickedKey.filter(keys=>!Guess.includes(keys)).length
-  console.log(WrongGuessKey)
-  function PressKey(letter){
-    setClickedKey(prevClickKey=>{
-      return prevClickKey.includes(letter)
-      ? prevClickKey : [...prevClickKey,letter]
-    })
-  }
-  const GuessWord = Guess.split("").map((letter,index)=>{
-    return (
-      <div key={index}>
-        {clickedKey.includes(letter) ? letter.toUpperCase():""}
-      </div>
+  ))
+  const GameWon = Guess.split("").every(letter=>clickedKey.includes(letter))
+
+  const GameLost = WrongGuessCount === people.length-1
+  
+  function PressKey(letter) {
+    setClickedKey(prevClickKey =>
+      prevClickKey.includes(letter)
+        ? prevClickKey
+        : [...prevClickKey, letter]
     )
-  })
-    
+  }
+
+  const GuessWord = Guess.split("").map((letter, index) => (
+    <div key={index}>
+      {clickedKey.includes(letter) ? letter.toUpperCase() : ""}
+    </div>
+  ))
+
   const keys = "abcdefghijklmnopqrstuvwxyz"
-  const keyboard = keys.split("").map(alphabet=>
-  {
-const isGuessed = clickedKey.includes(alphabet)
-const isCorrect = isGuessed && Guess.includes(alphabet)
-const isWrong = isGuessed && !Guess.includes(alphabet)
+  const keyboard = keys.split("").map(alphabet => {
+    const isGuessed = clickedKey.includes(alphabet)
+    const isCorrect = isGuessed && Guess.includes(alphabet)
+    const isWrong = isGuessed && !Guess.includes(alphabet)
+
     const className = clsx({
-      CorrectKey : isCorrect,
-      WrongKey : isWrong
+      CorrectKey: isCorrect,
+      WrongKey: isWrong
     })
+
     return (
-      <button className={className} onClick={()=>PressKey(alphabet)} key={alphabet}>
+      <button
+        className={className}
+        onClick={() => PressKey(alphabet)}
+        key={alphabet}
+      >
         {alphabet.toUpperCase()}
       </button>
     )
-  }
-  )
-  return(
+  })
+
+  return (
     <main>
-    <Header/>
-    <section className="Result-section">
+      <Header />
+      <section className="Result-section">
         <p>You saved humanity! </p>
-          <p>The humans survive!🥳</p>
-    </section>
-    <section className="Rivals">
-      {content}
-    </section>
-    <section className="Guess-section">
-      {GuessWord}
-    </section>
-    <section className="Keyboard-section">
-      {keyboard}
-    </section>
-    <button className="NewGame-Btn">New Game</button>
+        <p>The humans survive!🥳</p>
+      </section>
+      <section className="Rivals">{content}</section>
+      <section className="Guess-section">{GuessWord}</section>
+      <section className="Keyboard-section">{keyboard}</section>
+      {GameWon || GameLost ? <button className="NewGame-Btn">New Game</button>: null}
     </main>
   )
 }
